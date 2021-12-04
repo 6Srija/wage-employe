498f0c5b11d14b9e703dc99b486340eb19f964d1
6Srija <srijareddy6264@gmail.com>
2021-12-04 20:59:23 +0530

    added UC-8array

---
+++ UC-8array.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxRateInMonth=100
+empRatePerHr=20
+numOfWorkingDays=20
+totalEmpHr=0
+totalWorkingDays=0
+
+function getWorkingHrs()
+{
+            case $1 in
+                                $isFullTime)
+                                      empHrs=8
+                                      ;;
+                                $isPartTime)
+                                      empHrs=4
+                                      ;;
+                                      *)
+                                      empHrs=0
+                                      ;;
+esac
+}
+
+function getEmpWages()
+{
+                     echo $(($1*$empRatePerHr))
+}
+
+while [[ $totalEmpHr -lt $maxRateInMonth
+&& $totalWorkingDays -lt $numOfWorkingDays ]]
+do
+                     ((totalWorkingDays++))
+                     empCheck=$((RANDOM%3))
+                      getWorkingHrs $empCheck
+                      totalEmpHr=$(($totalEmpHr+$empHrs))
+                     dailyWages[$totalWorkingDays]=$(($empHrs+$empRatePerHr))
+done
+
+totalSalary="$( getEmpWages $totalEmpHr )"
+echo ${dailyWages[@]}
+
+
+
+
